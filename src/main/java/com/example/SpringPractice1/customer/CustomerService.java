ecaaaf156ace240185061f1cd4a6e7f26c4053c7
zh <ABottomCoder@github.com>
2024-11-13 21:02:06 -0800

    add log

--- src/main/java/com/example/SpringPractice1/customer/CustomerService.java
+++ src/main/java/com/example/SpringPractice1/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.example.SpringPractice1.customer;
 
 import com.example.SpringPractice1.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,9 @@ import java.util.List;
 
 @Service
 public class CustomerService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
     private final CustomerRepository customerRepository;
 
     @Autowired
@@ -17,12 +22,19 @@ public class CustomerService {
     }
 
     Customer getCustomer(Long id) {
-        return customerRepository.findById(id)
-                .orElseThrow(()->new NotFoundException(
-                        "Customer with id " + id + " not found"));
+        return customerRepository
+                .findById(id)
+                .orElseThrow(
+                        ()-> {
+                            NotFoundException notFoundException = new NotFoundException(
+                                    "Customer with id " + id + " not found");
+                            LOGGER.error("error in getting customer {}", id, notFoundException);
+                            return notFoundException;
+                        });
     }
 
     public List<Customer> getCustomers() {
+        LOGGER.info("getCustomers was called");
         return this.customerRepository.findAll();
     }
 }
