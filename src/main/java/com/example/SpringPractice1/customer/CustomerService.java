128e1e49b03b63aa52a82bbf788332a691ca29ba
zh <ABottomCoder@github.com>
2024-11-13 17:55:24 -0800

    add exception handler

--- src/main/java/com/example/SpringPractice1/customer/CustomerService.java
+++ src/main/java/com/example/SpringPractice1/customer/CustomerService.java
@@ -1,5 +1,6 @@
 package com.example.SpringPractice1.customer;
 
+import com.example.SpringPractice1.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,8 @@ public class CustomerService {
                 .stream()
                 .filter(customer -> customer.getId().equals(id))
                 .findFirst()
-                .orElseThrow(()->new IllegalArgumentException("Customer not found"));
+                .orElseThrow(()->new NotFoundException(
+                        "Customer with id " + id + " not found"));
     }
 
     public List<Customer> getCustomers() {
---
+++ src/main/java/com/example/SpringPractice1/exception/ApiRequestException.java
@@ -0,0 +1,11 @@
+package com.example.SpringPractice1.exception;
+
+public class ApiRequestException extends RuntimeException {
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
